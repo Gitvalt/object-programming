91479da6802364c04c9ff9fd92013ec8c34663fd
gitvalt <valtteri.seuranen@gmail.com>
2016-01-20 17:48:43 +0200

    20.1.2016 17:48

--- Demo3_harj/Program.cs
+++ Demo3_harj/Program.cs
@@ -6,10 +6,58 @@ using System.Threading.Tasks;
 
 namespace Demo3_harj
 {
+    class Car
+    {
+
+        //Readonly kuuluu esim. sarjanumeroille
+        public Car(string model, int cc)
+        {
+            this.model = model;
+            this.cc = cc;
+        }
+        public int EngineSize
+        {
+            get { return cc; }
+        }
+
+        public string ModelProperty
+        {
+            get { return model; }
+            set {
+                if(value =="corolla" || value == "avensis")
+                {
+                    model = value;
+                }
+                else
+                {
+                    model = "unknown";
+                }
+                     }
+        }
+        private string model;
+        private readonly int cc;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Car[] toyotas = new Car[1000];
+
+            for(int i=0; i < 1000; i++)//luodaan tuhat avensis autoa
+            {
+                toyotas[i] = new Car("avensis", 1895);
+            }
+
+            Car toyota = new Car("avensis", 1895);
+            Car toyota2 = new Car("corolla", 1599);
+            Car toyota3 = new Car("gt86", 1999);
+
+            toyota.ModelProperty = "corolla";
+
+            Console.WriteLine("This toyota model is: " + toyota.ModelProperty);
+            //Console.WriteLine("This toyota engine size is: " + toyota.cc);
+            Console.ReadLine();
         }
     }
 }
