123940bc2dfe3cf438b3a8169492c4256ee693da
K1967 <valtteri.seuranen@gmail.com>
2016-01-25 01:25:25 +0200

    Paivitetyt

--- Tehtava5/Program.cs
+++ Tehtava5/Program.cs
@@ -8,16 +8,40 @@ namespace Tehtava5
 {
     class Oppilas
     {
-        public Oppilas(string etunimi, string sukunimi, string ryhma)
+        public Oppilas(string etunimi, string sukunimi, string ryhma, int opintopisteet)
         {
             this.etunimi = etunimi;
             this.sukunimi = sukunimi;
             this.ryhma = ryhma;
+            this.opintopisteet = opintopisteet;
         }
 
-        private string etunimi;
-        private string sukunimi;
-        private string ryhma;
+        public void nimenvaihto()
+        {
+            Console.WriteLine("Nimen vaihtaminen: \nEtunimi:");
+            string enimi = Console.ReadLine();
+
+            
+
+            Console.WriteLine("Sukunimi:");
+            string snimi = Console.ReadLine();
+            Console.WriteLine("Uusi nimi on:");
+            etunimi = enimi;
+            sukunimi = snimi;
+            Console.WriteLine(etunimi + " " + sukunimi);
+
+
+
+        }
+
+        //Muuta ryhmaa
+        //vaihda nimi
+        //Keksi lisaa ominaisuuksia
+
+        public string etunimi;
+        public string sukunimi;
+        public string ryhma;
+        public int opintopisteet;
 
         
     }
@@ -25,12 +49,51 @@ namespace Tehtava5
     {
         static void Main(string[] args)
         {
-            Oppilas op1 = new Oppilas("Timo", "Joku", "TTV34S");
-            Oppilas op2 = new Oppilas("Heikki", "Makinen", "TTV34S");
-            Oppilas op3 = new Oppilas("Johanna", "Nieminen", "TTV35P");
-            Oppilas op4 = new Oppilas("Matti", "Mattinen", "TTV35S");
-            Oppilas op5 = new Oppilas("Tommi", "Hakkeri", "TTV34S");
+            Oppilas op1 = new Oppilas("Timo", "Joku", "TTV34S", 68);
+            Oppilas op2 = new Oppilas("Heikki", "Makinen", "TTV34S", 64);
+            Oppilas op3 = new Oppilas("Johanna", "Nieminen", "TTV35P", 18);
+            Oppilas op4 = new Oppilas("Matti", "Mattinen", "TTV35S", 22);
+            Oppilas op5 = new Oppilas("Tommi", "Hakkeri", "TTV34S", 70);
 
+            
+            string[] op1t = new string[] { op1.etunimi, op1.sukunimi, op1.ryhma, Convert.ToString(op1.opintopisteet) };
+            string[] op2t = new string[] { op2.etunimi, op2.sukunimi, op2.ryhma, Convert.ToString(op2.opintopisteet) };
+            string[] op3t = new string[] { op3.etunimi, op3.sukunimi, op3.ryhma, Convert.ToString(op3.opintopisteet) };
+            string[] op4t = new string[] { op4.etunimi, op4.sukunimi, op4.ryhma, Convert.ToString(op4.opintopisteet) };
+            string[] op5t = new string[] { op5.etunimi, op5.sukunimi, op5.ryhma, Convert.ToString(op5.opintopisteet) };
+            
+
+            string[,] opts = new string[5,4];
+            Console.WriteLine(op1t[1]);
+            Console.WriteLine(op1t[2]);
+            Console.WriteLine(op1t[3]);
+            Console.WriteLine(op1t[4]);//miksi ei
+            /*
+            for(int i=1; i < 5; i++)
+            {
+                opts[1, i] = op1t[i];
+            }
+            
+            Console.WriteLine(opts[1, 1] + "\n" + opts[1, 2] + "\n" + opts[1, 3] + "\n" + opts[1, 4]);
+            */
+
+
+
+
+
+
+
+
+
+            /*
+            foreach (string name in op1t) {
+                Console.WriteLine(name);
+            }
+            */
+
+
+
+            Console.ReadLine();
 
 
         }
--- tehtava4/Program.cs
+++ tehtava4/Program.cs
@@ -15,6 +15,7 @@ namespace tehtava4
             this.tyres = tyres;
         }
 
+
         public void PrintData()
         {
             Console.WriteLine("Nimi: " + name);
@@ -23,10 +24,45 @@ namespace tehtava4
 
         }
 
-        public void ToString()
+        public void Muutatieto()
         {
-            string yht = name + " " + speed + " " + tyres;
-            Console.WriteLine(yht);
+            bool jatka = true;
+
+            do
+            {
+                Console.WriteLine("1. Muuta nimi\n2. Muuta nopeus\n3. Muuta rengas tyyppi\n4. lopeta muuttaminen");
+                int input2 = int.Parse(Console.ReadLine());
+                switch (input2)
+                {
+                    case 1:
+                        Console.WriteLine("Uusi nimi: ");
+                        string unimi = Console.ReadLine();
+                        name = unimi;
+                        Console.WriteLine("Nimi on nyt: " + name);
+                        break;
+                    case 2:
+                        Console.WriteLine("Uusi nopeus: ");
+                        int unopeus = int.Parse(Console.ReadLine());
+                        speed = unopeus;
+                        Console.WriteLine("Nopeus on nyt: " + speed);
+                        break;
+                    case 3:
+                        Console.WriteLine("Uusi rengas tyyppi: ");
+                        int utyyppi = int.Parse(Console.ReadLine());
+                        tyres = utyyppi;
+                        Console.WriteLine("Rengas tyyppi on nyt: " + tyres);
+                        break;
+                    case 4:
+                        jatka = false;
+                        break;
+                }
+            } while (jatka == true);
+            
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ": " + "Nimi: " + name.ToString() + " Nopeus: " + speed.ToString() + " Renkaat: " + tyres.ToString();
         }
 
         private string name;
@@ -42,18 +78,74 @@ namespace tehtava4
             Console.WriteLine("\n");
             vh1.ToString();
             */
+
+            bool TT = false;
+            /*
             Console.WriteLine("Kirjoita uuden ajoneuvon ominaisuudet: \nnimi: ");
             string inputnim = Console.ReadLine();
 
-            Console.WriteLine("Nopeus: ");
-            int inputnopeus = int.Parse(Console.ReadLine());
+            bool nimi = String.IsNullOrEmpty(inputnim);*/
+            bool tester1 = true;
 
-            Console.WriteLine("Rengas tyyppi: ");
+            Console.WriteLine("Kirjoita uuden ajoneuvon ominaisuudet: \nNimi: ");
+            string inputnim = Console.ReadLine();
+            bool nimi = String.IsNullOrEmpty(inputnim);
+            if (nimi)
+            {
+                do
+                {
+                    Console.WriteLine("Kirjoita uuden ajoneuvon ominaisuudet: \nNimi: ");
+                    inputnim = Console.ReadLine();
+                    nimi = String.IsNullOrEmpty(inputnim);
+
+                    if (nimi == false)
+                    {
+                        Console.WriteLine("Nimi on: " + inputnim + "\n");
+                        tester1 = false;
+                    }
+
+
+                } while (tester1 == true);
+            }
+            
+
+            Console.WriteLine("\nNopeus: ");
+            int inputnopeus = int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("\nRengas tyyppi: ");
             int inputrengas = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
 
             vehicle vh1 = new vehicle(inputnim, inputnopeus, inputrengas);
-            vh1.PrintData();
-            vh1.ToString();
+
+
+            do
+            {
+                
+                Console.WriteLine("0. Lopeta ohjelma\n1. Tulosta vehicle ominaisuudet\n2. Merkkijonon tulostus\n3. Tietojen muuttaminen");
+                int input = int.Parse(Console.ReadLine());
+
+                switch (input) {
+                    case 0:
+                    TT = true;
+                    break;
+                    case 1:
+                    vh1.PrintData();
+                        
+                    break;
+                    case 2:
+                        Console.WriteLine(vh1.ToString());
+                    break;
+
+                    case 3:
+                        vh1.Muutatieto();
+                        break;
+
+                }
+                
+            } while (TT != true);
+            Console.WriteLine("Ohjelma lopetetaan");
 
             Console.ReadLine();
 
