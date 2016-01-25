55ab980028dd4aa6436082ae2f94f60d2952f021
K1967 <valtteri.seuranen@gmail.com>
2016-01-25 23:59:34 +0200

    25.1 yo

--- Tehtava5/Program.cs
+++ Tehtava5/Program.cs
@@ -47,6 +47,7 @@ namespace Tehtava5
     }
     class Program
     {
+     
         static void Main(string[] args)
         {
             Oppilas op1 = new Oppilas("Timo", "Joku", "TTV34S", 68);
@@ -55,43 +56,146 @@ namespace Tehtava5
             Oppilas op4 = new Oppilas("Matti", "Mattinen", "TTV35S", 22);
             Oppilas op5 = new Oppilas("Tommi", "Hakkeri", "TTV34S", 70);
 
-            
-            string[] op1t = new string[] { op1.etunimi, op1.sukunimi, op1.ryhma, Convert.ToString(op1.opintopisteet) };
-            string[] op2t = new string[] { op2.etunimi, op2.sukunimi, op2.ryhma, Convert.ToString(op2.opintopisteet) };
-            string[] op3t = new string[] { op3.etunimi, op3.sukunimi, op3.ryhma, Convert.ToString(op3.opintopisteet) };
-            string[] op4t = new string[] { op4.etunimi, op4.sukunimi, op4.ryhma, Convert.ToString(op4.opintopisteet) };
-            string[] op5t = new string[] { op5.etunimi, op5.sukunimi, op5.ryhma, Convert.ToString(op5.opintopisteet) };
+            string piste1 = Convert.ToString(op1.opintopisteet);
+            string piste2 = Convert.ToString(op2.opintopisteet);
+            string piste3 = Convert.ToString(op3.opintopisteet);
+            string piste4 = Convert.ToString(op4.opintopisteet);
+            string piste5 = Convert.ToString(op5.opintopisteet);
+
+
+
+            string[] op1t = new string[] { op1.etunimi, op1.sukunimi, op1.ryhma, piste1 };
+            string[] op2t = new string[] { op2.etunimi, op2.sukunimi, op2.ryhma, piste2 };
+            string[] op3t = new string[] { op3.etunimi, op3.sukunimi, op3.ryhma, piste3 };
+            string[] op4t = new string[] { op4.etunimi, op4.sukunimi, op4.ryhma, piste4 };
+            string[] op5t = new string[] { op5.etunimi, op5.sukunimi, op5.ryhma, piste5 };
             
 
             string[,] opts = new string[5,4];
+           /* Console.WriteLine(op1t[0]);
             Console.WriteLine(op1t[1]);
             Console.WriteLine(op1t[2]);
             Console.WriteLine(op1t[3]);
-            Console.WriteLine(op1t[4]);//miksi ei
-            /*
-            for(int i=1; i < 5; i++)
+            Console.WriteLine(piste1);//miksi ei == [0] on Timo, [3] == opintopisteet
+            */
+
+            //Tietojen laittaminen taulukkoon
+            for(int i=0; i < 4; i++)
             {
-                opts[1, i] = op1t[i];
+                opts[0, i] = op1t[i];
             }
-            
-            Console.WriteLine(opts[1, 1] + "\n" + opts[1, 2] + "\n" + opts[1, 3] + "\n" + opts[1, 4]);
-            */
 
+            for (int i = 0; i < 4; i++)
+            {
+                opts[1, i] = op2t[i];
+            }
 
+            for (int i = 0; i < 4; i++)
+            {
+                opts[2, i] = op3t[i];
+            }
 
+            for (int i = 0; i < 4; i++)
+            {
+                opts[3, i] = op4t[i];
+            }
 
-
-
-
-
+            for (int i = 0; i < 4; i++)
+            {
+                opts[4, i] = op5t[i];
+            }
 
             /*
-            foreach (string name in op1t) {
-                Console.WriteLine(name);
-            }
+           
             */
+            bool valmis = false;
+            int oppilas;
+
+            do
+            {
+                Console.WriteLine("1. Tulosta olemassa olevat oppilaat\n2. Muuta tietoja\n3. Lopeta ohjelma");
+                int console = int.Parse(Console.ReadLine());
+                int limiter = 4;
+                switch (console)
+                {
+
+                    case 1:
+                        string name = "";
+                        for (int i = 0; i < limiter; i++)
+                        {
+                            for (int j = 0; j < limiter; j++)
+                            {
+                                name += opts[i, j];
+                                name += " ";
+                                if (j == 3)
+                                {
+                                    Console.WriteLine(name);
+                                    name = "";
+                                }
 
 
+                            }
+                            Console.WriteLine("\n");
+                        }
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Valitse oppilas: ");
+                        for(int p = 0; p < 4; p++)
+                        {
+                            Console.WriteLine(p + ". " + opts[p, 0] + " " + opts[p, 1]);
+                        }
+                        Console.WriteLine("4. Cancel");
+                        int valinta = int.Parse(Console.ReadLine());//Oppilaan numero taulukossa x,
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Oppilas: " + opts[valinta,0] + " " + opts[valinta, 1]);
+                        Console.WriteLine("Oppilaan ryhma: " + opts[valinta, 2] + " Opintopisteet: " + opts[valinta, 3]);
+
+                        bool loopperi = false;
+                        do {
+                            Console.WriteLine("1. Muuta nimi\n2. Muuta ryhma\n3. Muuta opintopisteita\n4. Poistu");
+                            int lue = int.Parse(Console.ReadLine());
+
+                            switch (lue)
+                            {
+                                case 1:
+                                    Console.WriteLine("Uusi etunimi: ");
+                                    string usenimi = Console.ReadLine();
+                                    Console.WriteLine("Uusi sukunimi: ");
+                                    string ussnimi = Console.ReadLine();
+                                    opts[valinta, 0] = usenimi;
+                                    opts[valinta, 1] = ussnimi;
+                                    Console.WriteLine("Uusi nimi: " + opts[valinta, 0] + " " + opts[valinta, 1]);
+                                    break;
+
+                                case 2:
+                                    Console.WriteLine("Uusi ryhma: ");
+                                    string uryhma = Console.ReadLine();
+                                    opts[valinta, 2] = uryhma;
+                                    Console.WriteLine("Uusi ryhma on: " + opts[valinta, 2]);
+
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Uusi opintopisteiden maara: ");
+                                    int uoppiste = int.Parse(Console.ReadLine());
+                                    opts[valinta, 3] = Convert.ToString(uoppiste);
+                                    Console.WriteLine("Uusi pistemaara on: " + opts[valinta, 3]);
+
+                                    break;
+                                case 4:
+                                    loopperi = true;
+                                    break;
+                            }
+
+                        } while (loopperi != true);
+                        break;
+                    case 3:
+                        Console.WriteLine("Ohjelma lopetetaan");
+                        valmis = true;
+                        break;
+                }
+
+            } while (valmis == false);
 
             Console.ReadLine();
 
